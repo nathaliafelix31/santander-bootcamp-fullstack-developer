e284a4493881c16a6d4ee08c2b48428ee626e2b1
Nathália Félix <55997404+nathaliafelix31@users.noreply.github.com>
2022-07-13 12:09:02 -0300

    SpringBoot Security  com JWT

---
+++ Adicionando Segurança a uma API REST com Spring Security/dio-spring-security-jwt/src/main/java/dio/diospringsecurityjwt/controller/UserController.java
@@ -0,0 +1,20 @@
+package dio.diospringsecurityjwt.controller;
+
+import dio.diospringsecurityjwt.model.User;
+import dio.diospringsecurityjwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
---
+++ Adicionando Segurança a uma API REST com Spring Security/dio-spring-security-jwt/src/main/java/dio/diospringsecurityjwt/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package dio.diospringsecurityjwt.repository;
+
+import dio.diospringsecurityjwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+
+    boolean existsByUsername(String username);
+}
